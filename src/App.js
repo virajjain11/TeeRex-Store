b1527c2a839bf82b6d8398e22edacbe5faec0234
Viraj <virajjain7.vj@gmail.com>
2022-08-01 11:21:23 +0530

    Product card, filter setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Filter from "./components/Navbar/Product/Filter";
+import Productcard from "./components/Navbar/Product/Productcard";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        " https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
+      );
+      const newData = await res.json();
+      setData(newData);
+      // log
+    };
+    fetchData();
+    console.log(data);
+  }, []);
   return (
     <div className="lg:w-4/5 m-auto">
       {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
       <Navbar />
+      <div className="flex justify-around">
+        <Filter />
+
+        <Productcard />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Product/Productcard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Productcard = () => {
+  return <div>Product card</div>;
+};
+
+export default Productcard;
