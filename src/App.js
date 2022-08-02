e7214c0f1c722c423a73c483ad632cf908d4c849
Viraj <virajjain7.vj@gmail.com>
2022-08-02 20:16:00 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Product from "./components/Products/Product";
 import { fetchProduct } from "./store/ProductSlice";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -15,7 +16,7 @@ function App() {
     <div className="lg:w-11/12 m-auto">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/product" />} />
+        <Route path="*" element={<Navigate to="/product" />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
@@ -24,3 +25,13 @@ function App() {
 }
 
 export default App;
+
+//search any item
+// for (let k in obj) {
+//   if (obj[k] === "test1") {
+//       return true;
+//   }
+// }
+
+// var theObj = { foo: "bar" };
+// var hasVal = Object.values(theObj).includes("bar");
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+  // const handlePush = () => {
+  //   navigate("/");
+  // };
   return (
     <div className="flex justify-between p-4 bg-slate-300 items-center">
-      <h1 className="text-xl font-semibold tracking-wider">Tee Rex Store</h1>
+      <h1
+        // onClick={handlePush}
+        className="text-xl font-semibold tracking-wider cursor-pointer"
+      >
+        <Link to={"/"}>Tee Rex Store</Link>
+      </h1>
       <div className=" ">
         {/* <span className="pl-2">Products</span>
         <span className="pl-2">Cart</span> */}
         <nav>
-          <Link to="product"> Product</Link>
-          <Link to="cart">Cart</Link>
+          <Link className="pl-2" to="product">
+            {" "}
+            Product
+          </Link>
+          <Link className="pl-2" to="cart">
+            Cart
+          </Link>
         </nav>
       </div>
     </div>
