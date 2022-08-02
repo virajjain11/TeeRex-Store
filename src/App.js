83dbc3946d0316a6ad809bbba9ac1c9f531de378
Viraj <virajjain7.vj@gmail.com>
2022-08-02 14:05:40 +0530

    add routing package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
 import Navbar from "./components/Navbar/Navbar";
-import Filter from "./components/Navbar/Product/Filter";
-import Productcard from "./components/Navbar/Product/Productcard";
+import Product from "./components/Products/Product";
 import { fetchProduct } from "./store/ProductSlice";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   const [data, setData] = useState([]);
 
@@ -13,19 +13,14 @@ function App() {
     dispatch(fetchProduct());
   }, [dispatch]);
 
-  const products = useSelector((state) => state.product.store);
   return (
     <div className="lg:w-11/12 m-auto">
-      <Navbar />
-      <div className="flex ">
-        <Filter products={products} />
-        <div className="flex flex-wrap">
-          {products?.length > 0 &&
-            products.map((product) => (
-              <Productcard key={product.id} product={product} />
-            ))}
-        </div>
-      </div>
+      <Routes>
+        {/* <Route /> */}
+        <Navbar />
+        <Product />
+        <Cart />
+      </Routes>
     </div>
   );
 }
