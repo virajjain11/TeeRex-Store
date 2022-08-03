ea1ff18eeb56017fe39b8a28497327dd68ae64a3
Viraj <virajjain7.vj@gmail.com>
2022-08-03 20:38:56 +0530

    create cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import CartCard from "./CartCard";
 
 const Cart = () => {
-  return <div>cart</div>;
+  const cartItems = useSelector((state) => state.product.cart);
+  console.log(cartItems);
+  return (
+    <div className="m-4">
+      <h1 className="text-xl font-medium tracking-wide">Shopping Cart</h1>
+      <div className="mt-10">
+        {cartItems?.length > 0 &&
+          cartItems.map((el, idx) => {
+            return <CartCard key={idx} item={el} />;
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
