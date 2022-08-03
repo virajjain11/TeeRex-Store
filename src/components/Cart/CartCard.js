68d52850194e02c4090229b1ca3ce750504404ef
Viraj <virajjain7.vj@gmail.com>
2022-08-03 20:39:24 +0530

    add styling to cart items

---
+++ src/components/Cart/CartCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CartCard = ({ item }) => {
+  return (
+    <>
+      <div className="flex justify-around content-center md:space-x-2 sm:justify-start sm:mx-0 max-w-[450px] sm:max-w-[600px]">
+        <div className=" max-w-[90px] sm:max-w-[150px] flex content-center justify-center">
+          <img src={item.imageURL} alt="img" />
+        </div>
+        <div className="p-2  flex flex-col content-center justify-center font-semibold ">
+          <span className="px-2">{item.name}</span>
+          <span className="px-2">Rs {item.price}</span>
+        </div>
+        <div className=" sm:mx-2 my-auto">
+          <span className=" py-2 sm:px-6 px-1 rounded-sm  bg-slate-300 font-semibold text-center">
+            Qty {item.quantity}
+          </span>
+        </div>
+        <div className="flex">
+          <button className="border sm:px-6 px-2 py-2 rounded-sm font-semibold sm:mx-4 mx-1  my-auto">
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartCard;
