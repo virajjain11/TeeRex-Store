b618159c1dfb9738800a305c082f294249478697
Viraj <virajjain7.vj@gmail.com>
2022-08-05 00:12:31 +0530

    styling WIP

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -6,11 +6,11 @@ const Product = () => {
   const products = useSelector((state) => state.product.store);
   return (
     <>
-      <div className="flex justify-center ml-6 ">
-        {/* <div className=""> */}
-        <Filter />
-        {/* </div> */}
-        <div className="flex flex-wrap">
+      <div className="flex justify-center sm:ml-6 ">
+        <div className="w-full sm:mr-6 md:mr-10">
+          <Filter />
+        </div>
+        <div className="flex sm:ml-4 flex-wrap">
           {products?.length > 0 &&
             products.map((product) => (
               <Productcard key={product.id} product={product} />
